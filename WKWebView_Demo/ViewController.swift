fc71811304bed8dddbacd6007eecbe92cefed0cc
Ayaan Ruhi <parth@Parths-MacBook-Pro.local>
2018-10-11 01:35:24 -0500

    Second Commit(Update some code)

--- WKWebView_Demo/ViewController.swift
+++ WKWebView_Demo/ViewController.swift
@@ -7,19 +7,40 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        loadWebView()
+        addKVObserver()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func loadWebView() {
+        let urlString = "https://www.apple.com"
+        let url = URL(string: urlString)
+        let request = URLRequest(url: url!)
+        self.webView.load(request)
     }
-
+    // MARK: Add key value observer when webview is loading
+    func addKVObserver() {
+        self.webView.addObserver(self, forKeyPath: #keyPath(WKWebView.isLoading), options: .new, context: nil)
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "loading" {
+            if webView.isLoading {
+                activityIndicator.startAnimating()
+                activityIndicator.isHidden = false
+            } else {
+                activityIndicator.stopAnimating()
+            }
+        }
+    }
 
 }
 
